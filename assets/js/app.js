dc21d2648b42661c11cb60c0800fd7636981002d
Vizena2005 <vizena2005@gmail.com>
2018-03-13 18:17:34 -0500

    final func push

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,7 @@ $(document).ready(function () {
     // Global vars
     var database = firebase.database();
     var ref = database.ref("trains");
-    var time = moment().format('LT');
     // 
-
     $("#addTrainBtn").on("click", function (e) {
         e.preventDefault();
         // Sets vars that calls and trims the inputs from the user
@@ -13,12 +11,23 @@ $(document).ready(function () {
         var firstTrainTime = $("#firstTrainTimeInput").val().trim();
         var frequency = $("#frequencyInput").val().trim();
         // 
+        // conversion
+        firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+        currentTime = moment();
+        diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        tRemainder = diffTime % frequency;
+        minutesUntilTrain = frequency - tRemainder;
+        nextTrain = moment().add(minutesUntilTrain, "minutes");
+        nextTrainFormatted = moment(nextTrain).format("hh:mm");
+        // 
         // Making a obj called train
         var train = {
             trainName: trainName,
             destination: destination,
             firstTrainTime: firstTrainTime,
-            frequency: frequency
+            frequency: frequency,
+            nextTrainFormatted: nextTrainFormatted,
+            minutesUntilTrain: minutesUntilTrain
         }
         ref.push(train);
         // 
@@ -31,15 +40,17 @@ $(document).ready(function () {
         var trains = train.val();
         var keys = Object.keys(trains);
         // 
+        $("#tableBody").empty();
         for (i = 0; i < keys.length; i++) {
             var tRow = $("<tr>");
             var k = keys[i];
             // These 2 work fine its simple copy and paste to the dom
             $("<td scope='col'>").text(trains[k].trainName).appendTo(tRow);
             $("<td scope='col'>").text(trains[k].destination).appendTo(tRow);
-            $("<td scope='col'>").text(trains[k].frequency + " min").appendTo(tRow);
+            $("<td scope='col'>").text(trains[k].frequency).appendTo(tRow);
             // These I need to add math to prior to DOM injection
-            $("<td scope='col'>").text(trains[k].firstTrainTime).appendTo(tRow);
+            $("<td scope='col'>").text(trains[k].nextTrainFormatted).appendTo(tRow);
+            $("<td scope='col'>").text(trains[k].minutesUntilTrain).appendTo(tRow);
 
             $("#tableBody").append(tRow);
         };
